11c7ef80f405951d77befb2cf19f9ae043d30f06
pylardotco <pylarco@gmail.com>
2025-07-30 15:51:22 +0700

    initialized

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  target: 'es2022',
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  splitting: false,
+  treeshake: true,
+  minify: false,
+  outDir: 'dist',
+  external: ['repograph-browser'],
+});
